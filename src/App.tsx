f0415a773a37bc6b7076e508d9a5bcc72790f1e5
nd-02110114 <nd.12021218@gmail.com>
2018-08-28 21:37:12 +0900

    :sparkles: (src) introduce navigation & atomic design  structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,97 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+import { createBottomTabNavigator } from 'react-navigation'
+import { TestA, TestB } from './pages'
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${API_TOKEN}`
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+client
+  .query({
+    query: gql`
+      {
+        search(
+          first: 1
+          query: "language: javascript good-first-issues:>1 stars:>500"
+          type: REPOSITORY
+        ) {
+          repositoryCount
+          pageInfo {
+            startCursor
+            endCursor
+            hasNextPage
+          }
+          nodes {
+            ... on Repository {
+              owner {
+                id
+                avatarUrl
+                login
+                url
+              }
+              id
+              description
+              name
+              url
+              issues(
+                first: 100
+                labels: ["good first issue"]
+                states: OPEN
+                orderBy: { field: UPDATED_AT, direction: DESC }
+              ) {
+                totalCount
+                nodes {
+                  title
+                  url
+                  author {
+                    avatarUrl
+                  }
+                  updatedAt
+                }
+              }
+              stargazers {
+                totalCount
+              }
+            }
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
+  .catch(e => console.log(e))
+
+const Root = createBottomTabNavigator({
+  searchedList: TestA,
+  watchedList: TestB
+})
 
 export default class App extends React.Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up src/App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <Root />
+      </ApolloProvider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
