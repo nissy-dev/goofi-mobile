061a246ab0fca6a19b2daeb25797c1b91da423c2
nd-02110114 <nd.12021218@gmail.com>
2019-01-08 10:15:22 +0900

    :recycle: (src) create favriteheader component

--- src/organisms/header/index.tsx
+++ src/organisms/header/index.tsx
@@ -1,3 +1,4 @@
-export { default as SearchHeader } from './SearchHeader'
+export { default as FavoriteHeader } from './FavoriteHeader'
 export { default as IssueHeader } from './IssueHeader'
+export { default as SearchHeader } from './SearchHeader'
 export { default as WebViewHeader } from './WebViewHeader'
--- src/pages/favorite/index.tsx
+++ src/pages/favorite/index.tsx
@@ -1,32 +1,12 @@
 import * as React from 'react'
-import styled from 'styled-components/native'
-import { View } from 'react-native'
-import { Container, Heading } from '../../atoms'
-import I18n from '../../locale'
-import { WHITE, BASE_COLOR } from '../../../assets'
+import { Container } from '../../atoms'
+import { FavoriteHeader } from '../../organisms'
 
-const Header = styled(View)`
-  flex-direction: column;
-  align-items: center;
-  padding-top: 30;
-  width: 100%;
-  height: 80;
-  background-color: ${BASE_COLOR};
-`
-
-interface Props {
-  data: any
-}
-
-export default class FavoritePage extends React.Component<Props, {}> {
+export default class FavoritePage extends React.Component<{}, {}> {
   render() {
     return (
       <Container>
-        <Header>
-          <Heading size={25} color={WHITE}>
-            {I18n.t('favorite')}
-          </Heading>
-        </Header>
+        <FavoriteHeader />
         <Container />
       </Container>
     )
